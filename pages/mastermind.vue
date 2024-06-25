065a7ccf91d468fc5d528c15dd82a3ebd85a67ef
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-06-25 12:26:19 +0200

    made grid for mastermind combination

--- pages/mastermind.vue
+++ pages/mastermind.vue
@@ -1,1 +1,21 @@
-<template>mastermind</template>
+<template>
+    <main class="flex justify-center">
+        
+        <section class="board mt-12 bg-slate-200">
+            <div class="mastermind-grid divide-slate-300 divide-y">
+                <div class="row" v-for="n in 10">
+                    <div class="combination flex gap-4 p-2">
+                        <div class="size-7 rounded-full border bg-white" v-for="n in 4"></div>
+                    </div>
+                    <div class="result">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="mastermind-choice">
+                
+            </div>
+        </section>
+    </main>
+</template>
