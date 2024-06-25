df8520c317bbe9e6e4704e94fb8012ac88048560
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-06-25 12:51:03 +0200

    made mastermind result grid

--- pages/mastermind.vue
+++ pages/mastermind.vue
@@ -3,12 +3,12 @@
         
         <section class="board mt-12 bg-slate-200">
             <div class="mastermind-grid divide-slate-300 divide-y">
-                <div class="row" v-for="n in 10">
+                <div class="row flex divide-x divide-slate-300" v-for="n in 10">
                     <div class="combination flex gap-4 p-2">
                         <div class="size-7 rounded-full border bg-white" v-for="n in 4"></div>
                     </div>
-                    <div class="result">
-
+                    <div class="result flex flex-wrap justify-center w-14 p-2">
+                        <div class="size-4 rounded-full border bg-white" v-for="n in 4"></div>
                     </div>
                 </div>
             </div>
